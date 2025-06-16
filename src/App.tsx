3cf0b7ac276e763fc6776a10a3f195e2e381098d
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-06-16 17:13:52 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  
+  return (
+    <>
+      <h2>hello</h2>
+      <Button className="bg-red-400 cursor-pointer">Button</Button>
+    </>
+  )
+}
+
+export default App
