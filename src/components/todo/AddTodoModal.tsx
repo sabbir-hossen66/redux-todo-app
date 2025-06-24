927c691818e4d20e31998c5759b4de8493439cf6
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-06-24 17:40:27 +0600

    form add

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -11,12 +11,22 @@ import {
 } from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label";
 import { Input } from "../ui/input";
+import { useState, type FormEvent } from "react";
+import { CloudCog } from "lucide-react";
 
 
 const AddTodoModal = () => {  
+  const [tast, setTast] = useState('');
+  const [description, setDescription] = useState('');
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  console.log({tast, description});
+  }
+
   return (
     <Dialog>
-      <form>
+      
         <DialogTrigger asChild>
           <Button variant="outline">Open Dialog</Button>
         </DialogTrigger>
@@ -28,24 +38,26 @@ const AddTodoModal = () => {
               done.
             </DialogDescription>
           </DialogHeader>
-          <div className="grid gap-4">
+          <form onSubmit={onSubmit}>
+          <div  className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              <Label htmlFor="task">Task</Label>
+              <Input onBlur={(e)=>setTast(e.target.value)} id="task" name="task" />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
+              <Label htmlFor="description">description</Label>
+              <Input onBlur={(e)=>setDescription(e.target.value)} id="username-1" name="username"  />
             </div>
           </div>
-          <DialogFooter>
+          <div>
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
+            <Button  variant={"outline"} type="submit">Save changes</Button>
+          </div>
       </form>
+      </DialogContent>
+      
     </Dialog>
   )
 }
