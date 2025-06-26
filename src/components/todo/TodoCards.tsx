548d29c76d1c8732f18b6929c3e45a8ec15778dd
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-06-26 17:27:55 +0600

    add toggole function

--- src/components/todo/TodoCards.tsx
+++ src/components/todo/TodoCards.tsx
@@ -1,21 +1,33 @@
 import { useAppDispatch } from "@/redux/feature/hook";
 import { Button } from "../ui/button";
-import { removeTodo } from "@/redux/feature/todoSlice";
+import { removeTodo, toggleComplete } from "@/redux/feature/todoSlice";
 
 type TTodoCardsProps = {
   title: string;
   description: string;
   id?: string;
+  isCompleted?: boolean;
 }
 
-const TodoCards = ({ title, description,id }: TTodoCardsProps) => {
+const TodoCards = ({ title, description,id,isCompleted }: TTodoCardsProps) => {
   const dispatch = useAppDispatch();
+  const toggleState = () => {
+    dispatch(toggleComplete(id));
+  }
   return (
     <div className="bg-white rounded-md flex justify-between items-center p-3">
-    <input type="checkbox" name="" id="" />
+    <input onChange={toggleState} type="checkbox" name="complete" id="complete" />
       <p className="font-semibold">{title}</p>
       <p>{ description}</p>
   
+      <div>
+        {
+          isCompleted ?
+          <span className="text-green-500 font-semibold">Completed</span> :
+          <span className="text-red-500 font-semibold">Pending</span>
+        }
+      </div>
+      
   <div className="space-x-5">
 <Button onClick={()=>dispatch(removeTodo(id))} className="bg-red-500 "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4">
   <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
